7441714609ff684aca1e86bd5627e1e5c3932d97
Shaobro <517278936@qq.com>
2021-01-04 16:38:26 +0800

    Site updated: 2021-01-04 16:38:24

--- js/utils.js
+++ js/utils.js
@@ -68,6 +68,7 @@ NexT.utils = {
       element.wrap(box);
       box.classList.add('highlight-container');
       box.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-clipboard fa-fw"></i></div>');
+      console.log(box,123);
       var button = element.parentNode.querySelector('.copy-btn');
       button.addEventListener('click', event => {
         var target = event.currentTarget;
